7caafb4db21ff8ab0f8808833f6773bcf6982945
James D. Nurmi <james@abneptis.com>
2011-01-15 14:43:49 -0800

    Better unmarshalling behaviour for use by other netlink messages

--- misc/attribute.go
+++ misc/attribute.go
@@ -1,6 +1,8 @@
 package netlink
 
 import "os"
+import "bytes"
+import "encoding/binary"
 
 type AttributeType uint16
 
@@ -46,3 +48,44 @@ func UnmarshalAttribute(in []byte)(out Attribute, err os.Error){
   if err == nil { out = a }
   return
 }
+
+func UnmarshalAttributes(in []byte, padding int)(out []Attribute, err os.Error){
+  pos := 0
+  for pos < len(in) {
+    l := binary.LittleEndian.Uint16(in[pos:pos+2])
+    if int(l) > len(in) - pos {
+      err = os.NewError("Can't parse attribute (too long)")
+      break
+    }
+    if l > 0 {
+      t := binary.LittleEndian.Uint16(in[pos+2:pos+4])
+      out = append(out, attr{_type: AttributeType(t), _body:in[pos+4:l - 4]})
+      pos += int(l)
+      if padding > 0 {
+        if pos % padding != 0 { pos += padding - (pos % padding) }
+      }
+    } else {
+      err = os.NewError("Invalid attribute length.")
+      break
+    }
+  }
+  return
+}
+
+func MarshalAttributes(in []Attribute, padding int)(out []byte, err os.Error){
+  buff := bytes.NewBuffer(nil)
+  for i := range(in){
+    ahdr := [4]byte{}
+    body := in[i].Body()
+    binary.LittleEndian.PutUint16(ahdr[2:4], uint16(in[i].AttributeType()))
+    binary.LittleEndian.PutUint16(ahdr[0:2], uint16(len(body)) + 4)
+    buff.Write(ahdr[0:])
+    buff.Write(body)
+    if padding > 0 && len(body) % padding != 0 {
+      buff.Write(make([]byte, padding - (len(body) % padding)))
+    }
+  }
+  out = buff.Bytes()
+  return
+}
+

679dadd4a1fece72e9417c1a3696ff64a2359ae5
James D. Nurmi <james@abneptis.com>
2011-01-16 18:30:33 -0800

    Use header-styled for Route;  rename struct to Header

--- rtnetlink/route/message.go
+++ rtnetlink/route/message.go
@@ -1,54 +1,43 @@
 package route
 
 import "encoding/binary"
-import "netlink"
 import "netlink/rtnetlink"
 import "os"
 
 
-type Message struct {
-  header [12]byte
-  attributes []netlink.Attribute
-}
+type Header [12]byte
 
-func NewMessage(afam byte, dl uint8, sl uint8, tos uint8, t Table, o Origin, s rtnetlink.Scope, T Type, f Flags)(*Message){
-  hdr := [12]byte{afam, dl, sl, tos, byte(t), byte(o), byte(s), byte(T)}
+func NewMessage(afam byte, dl uint8, sl uint8, tos uint8, t Table, o Origin, s rtnetlink.Scope, T Type, f Flags)(*Header){
+  hdr := Header{afam, dl, sl, tos, byte(t), byte(o), byte(s), byte(T)}
   binary.LittleEndian.PutUint32(hdr[8:12], uint32(f))
-  return &Message{
-    header: hdr,
-  }
+  return &hdr
 }
 
-func (self Message)AddressFamily()(byte){ return self.header[0] }
-func (self Message)AddressDestLength()(uint8){ return self.header[1] }
-func (self Message)AddressSourceLength()(uint8){ return self.header[2] }
-func (self Message)TOS()(uint8){ return self.header[3] }
-func (self Message)RoutingTable()(Table){ return Table(self.header[4]) }
-func (self Message)RouteOrigin()(Origin){ return Origin(self.header[5]) }
-func (self Message)AddressScope()(rtnetlink.Scope){ return rtnetlink.Scope(self.header[6]) }
-func (self Message)RouteType()(Type){ return Type(self.header[7]) }
-func (self Message)Flags()(Flags) { return Flags(binary.LittleEndian.Uint32(self.header[8:12])) }
+func (self Header)AddressFamily()(byte){ return self[0] }
+func (self Header)AddressDestLength()(uint8){ return self[1] }
+func (self Header)AddressSourceLength()(uint8){ return self[2] }
+func (self Header)TOS()(uint8){ return self[3] }
+func (self Header)RoutingTable()(Table){ return Table(self[4]) }
+func (self Header)RouteOrigin()(Origin){ return Origin(self[5]) }
+func (self Header)AddressScope()(rtnetlink.Scope){ return rtnetlink.Scope(self[6]) }
+func (self Header)RouteType()(Type){ return Type(self[7]) }
+func (self Header)Flags()(Flags) { return Flags(binary.LittleEndian.Uint32(self[8:12])) }
 
-func (self Message)Attributes()([]netlink.Attribute) { return self.attributes }
-func (self *Message)AddAttribute(a netlink.Attribute)(){ self.attributes = append(self.attributes, a) }
 
-func (self *Message)UnmarshalNetlink(in []byte, pad int)(err os.Error){
+func (self *Header)UnmarshalNetlink(in []byte, pad int)(err os.Error){
   if len(in) < 12 {
     err = os.NewError("Too short to be a valid Routing Message")
   }
   if err == nil {
-    copy(self.header[0:12], in[0:12])
-    self.attributes, err = netlink.UnmarshalAttributes(in[12:], pad)
+    copy(self[0:12], in[0:12])
   }
   return
 }
 
-func (self Message)MarshalNetlink(pad int)(out []byte, err os.Error){
-  attrbytes, err := netlink.MarshalAttributes(self.attributes, pad)
+func (self Header)MarshalNetlink(pad int)(out []byte, err os.Error){
   if err == nil {
-    out = make([]byte, len(attrbytes) + 12)
-    copy(out, self.header[0:])
-    copy(out[12:], attrbytes)
+    out = make([]byte, 12)
+    copy(out, self[0:])
   }
   return
 }

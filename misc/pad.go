6221e03cee4e705ae89aaa9c5afa7d545085e11c
James D. Nurmi <james@abneptis.com>
2011-01-18 13:02:29 -0800

    Add pad/reposition as netlink functions

---
+++ misc/pad.go
@@ -0,0 +1,26 @@
+package netlink
+
+func PadBytes(in []byte, pad int)(out []byte){
+  if pad > 0 {
+    pblk := (len(in) + 1) / pad
+    fsize := pblk * pad
+    if fsize != len(in) {
+      out = make([]byte, fsize)
+      copy(out, in)
+    } else {
+      out = in
+    }
+  }
+  return
+}
+
+func Reposition(pos int, pad int)(out int){
+  if pad > 0 {
+    out = pad * ((pos + (pad - 1) ) / pad)
+  } else {
+    out = pos
+  }
+  return
+}
+
+

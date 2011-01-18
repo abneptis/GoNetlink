d3deac281cb8a48692c19402e43d6d40abc3d81c
James D. Nurmi <james@abneptis.com>
2011-01-18 13:06:39 -0800

    Use common/family, and pad marshal results

--- rtnetlink/link/header.go
+++ rtnetlink/link/header.go
@@ -1,5 +1,7 @@
 package link
 
+import "netlink"
+import "netlink/rtnetlink"
 import "os"
 import "encoding/binary"
 
@@ -18,12 +20,12 @@ func (self *Header)UnmarshalNetlink(in []byte, pad int)(err os.Error){
 }
 
 func (self Header)MarshalNetlink(pad int)(out []byte, err os.Error){
-  out = self[0:HEADER_LENGTH]
+  out = netlink.PadBytes(self[0:HEADER_LENGTH], pad)
   return
 }
 
 
-func (self Header)InterfaceFamily()(byte){ return self[0]}
+func (self Header)InterfaceFamily()(rtnetlink.Family){ return rtnetlink.Family(self[0])}
 func (self Header)InterfaceType()(uint16){ return binary.LittleEndian.Uint16(self[2:4]) }
 func (self Header)InterfaceIndex()(uint32){ return binary.LittleEndian.Uint32(self[4:8]) }
 func (self Header)InterfaceFlags()(Flags){ return Flags(binary.LittleEndian.Uint32(self[8:12])) }

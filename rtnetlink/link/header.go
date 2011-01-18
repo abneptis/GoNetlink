4ba5c836286bd74d1d79c4d9b1a5e3e75e882873
James D. Nurmi <james@abneptis.com>
2011-01-18 14:30:43 -0800

    Add a NewHeader function

--- rtnetlink/link/header.go
+++ rtnetlink/link/header.go
@@ -9,6 +9,15 @@ import "encoding/binary"
 const HEADER_LENGTH = 16
 type Header [16]byte
 
+func NewHeader(fam rtnetlink.Family, itype uint16, iindex uint32, flags, changes Flags)(hdr *Header){
+  hdr = &Header{byte(fam)}
+  binary.LittleEndian.PutUint16(hdr[2:4], itype)
+  binary.LittleEndian.PutUint32(hdr[4:8], iindex)
+  binary.LittleEndian.PutUint32(hdr[8:12], uint32(flags))
+  binary.LittleEndian.PutUint32(hdr[12:16], uint32(changes))
+  return
+}
+
 func (self Header)Len()(int) { return HEADER_LENGTH }
 func (self *Header)UnmarshalNetlink(in []byte, pad int)(err os.Error){
   if len(in) != HEADER_LENGTH {
@@ -23,7 +32,6 @@ func (self Header)MarshalNetlink(pad int)(out []byte, err os.Error){
   out = netlink.PadBytes(self[0:HEADER_LENGTH], pad)
   return
 }
-
 
 func (self Header)InterfaceFamily()(rtnetlink.Family){ return rtnetlink.Family(self[0])}
 func (self Header)InterfaceType()(uint16){ return binary.LittleEndian.Uint16(self[2:4]) }

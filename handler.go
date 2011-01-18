4104a16eeae7c9a78158c58eff465142df4b9021
James D. Nurmi <james@abneptis.com>
2011-01-18 13:08:52 -0800

    Misc API updates

--- handler.go
+++ handler.go
@@ -1,12 +1,13 @@
 package netlink
-
+//import "log"
+import "bufio"
 import "os"
 import "fmt"
 import "sync"
 
 type Handler struct {
   sock *Socket
-  recipients map[uint32]chan NetlinkMessage
+  recipients map[uint32]chan Message
   next_seq uint32
   lock sync.Mutex
 }
@@ -21,39 +22,42 @@ func (self *Handler)Seq()(out uint32){
 }
 
 func NewHandler(sock *Socket)(*Handler){
-  return &Handler{sock:sock, recipients: map[uint32]chan NetlinkMessage{}, next_seq: 1}
+  return &Handler{sock:sock, recipients: map[uint32]chan Message{}, next_seq: 1}
 }
 
-func (self *Handler)SendQuery(msg NetlinkMessage, ch chan NetlinkMessage)(err os.Error){
-  if msg.Sequence() == 0 {
-    msg.SetSequence(self.Seq())
+func (self *Handler)SendQuery(msg Message, ch chan Message)(err os.Error){
+  //log.Printf("Writing Message = %+v", msg.Header)
+  if msg.Header.MessageSequence() == 0 {
+    msg.Header.SetMessageSequence(self.Seq())
   }
-  if msg.Sequence() == 0 {
+  if msg.Header.MessageSequence() == 0 {
     return os.NewError("Failed to set sequence number")
   }
   var ob []byte
-  ob, err = Marshal(msg)
+  //log.Printf("msg = %v", msg)
+  ob, err = msg.MarshalNetlink(4)
+  //log.Printf("OB: %X; ERR: %v", ob, err)
   if err == nil {
-    self.recipients[msg.Sequence()] = ch
+    self.recipients[msg.Header.MessageSequence()] = ch
     _, err = self.sock.Write(ob)
   }
   return
 }
 
 func (self *Handler)Start(echan chan os.Error){
-  rdr := NewReader(self.sock)
+  r := bufio.NewReader(self.sock)
   for {
-    msg, err := rdr.Read();
+    msg, err := ReadMessage(r, 4)
     if err == nil {
-      if self.recipients[msg.Sequence()] == nil {
-        echan <- os.NewError(fmt.Sprintf("GoNetlink: No handler found for sequence number: %d", msg.Sequence()))
+      if self.recipients[msg.Header.MessageSequence()] == nil {
+        echan <- os.NewError(fmt.Sprintf("GoNetlink: No handler found for sequence number: %d", msg.Header.MessageSequence()))
         continue
       } else {
-        switch msg.MessageType() {
+        switch msg.Header.MessageType() {
           case NLMSG_DONE:
-            close(self.recipients[msg.Sequence()])
-            self.recipients[msg.Sequence()] = nil, false
-          default: self.recipients[msg.Sequence()] <- msg
+            close(self.recipients[msg.Header.MessageSequence()])
+            self.recipients[msg.Header.MessageSequence()] = nil, false
+          default: self.recipients[msg.Header.MessageSequence()] <- *msg
         }
       }
     } else {

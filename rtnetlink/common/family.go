33d42f3efd44425eee8abc3bb7b3208b3da182a2
James D. Nurmi <james@abneptis.com>
2011-01-18 13:35:24 -0800

    add Family/String() method

--- rtnetlink/common/family.go
+++ rtnetlink/common/family.go
@@ -10,3 +10,12 @@ const (
   AF_INET6  Family = syscall.AF_INET6
 )
 
+func (self Family)String()(out string){
+  switch self {
+    default: out = "Unknown"
+    case AF_UNSPEC: out = "AF_UNSPEC"
+    case AF_INET: out = "AF_INET"
+    case AF_INET6: out = "AF_INET6"
+  }
+  return
+}

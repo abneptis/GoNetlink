55f2f21462dc434a60a4043de4d49e1209c2a906
James D. Nurmi <james@abneptis.com>
2011-01-18 13:03:39 -0800

    Misc fixes, remove old marshalling calls

--- misc/attribute.go
+++ misc/attribute.go
@@ -1,26 +1,15 @@
 package netlink
 
+import "fmt"
 import "os"
 import "bytes"
 import "encoding/binary"
 
 type AttributeType uint16
 
-func (self *AttributeType)Unmarshal(in []byte)( err os.Error){
-  var mt uint16
-  err = Unmarshal(in, &mt)
-  if err == nil { *self = AttributeType(mt) }
-  return
-}
-
-
 type Attribute interface {
   AttributeType()(AttributeType)
   Body()([]byte)
-}
-
-func (self AttributeType)Marshal()([]byte, os.Error){
-  return Marshal(uint16(self))
 }
 
 type attr struct {
@@ -30,24 +19,6 @@ type attr struct {
 
 func (self attr)AttributeType()(AttributeType){ return self._type }
 func (self attr)Body()([]byte){ return self._body}
-
-func (self *attr)Unmarshal(in []byte)(err os.Error){
-  if len(in) < 2 {
-    return os.NewError("Cannot be valid attribute")
-  }
-  err = Unmarshal(in[0:2], &self._type)
-  if err == nil {
-    self._body = in[2:]
-  }
-  return
-}
-
-func UnmarshalAttribute(in []byte)(out Attribute, err os.Error){
-  a := &attr{}
-  err = a.Unmarshal(in)
-  if err == nil { out = a }
-  return
-}
 
 func UnmarshalAttributes(in []byte, padding int)(out []Attribute, err os.Error){
   pos := 0
@@ -60,12 +31,9 @@ func UnmarshalAttributes(in []byte, padding int)(out []Attribute, err os.Error){
     if l > 4 {
       t := binary.LittleEndian.Uint16(in[pos+2:pos+4])
       out = append(out, attr{_type: AttributeType(t), _body:in[pos+4:pos + int(l)]})
-      pos += int(l)
-      if padding > 0 && pos % padding != 0 {
-        pos += padding - (pos % padding)
-      }
+      pos = Reposition(pos + int(l), padding)
     } else {
-      err = os.NewError("Invalid attribute length.")
+      err = os.NewError(fmt.Sprintf("Invalid attribute (Len: %d):", l))
       break
     }
   }
@@ -81,11 +49,8 @@ func MarshalAttributes(in []Attribute, padding int)(out []byte, err os.Error){
     binary.LittleEndian.PutUint16(ahdr[0:2], uint16(len(body)) + 4)
     buff.Write(ahdr[0:])
     buff.Write(body)
-    if padding > 0 && len(body) % padding != 0 {
-      buff.Write(make([]byte, padding - (len(body) % padding)))
-    }
   }
-  out = buff.Bytes()
+  out = PadBytes(buff.Bytes(), padding)
   return
 }
 
